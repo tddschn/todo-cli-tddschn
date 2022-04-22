97735d76520000cb2583dd4def71a4ffcb71de26
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-04-22 15:22:18 +0800

    add logger

--- todo_cli_tddschn/__init__.py
+++ todo_cli_tddschn/__init__.py
@@ -1,1 +1,11 @@
 __version__ = '0.1.0'
+__app_name__ = 'todo'
+
+try:
+    from logging_utils_tddschn import get_logger
+    logger, _ = get_logger(__app_name__)
+except:
+    import logging
+    from logging import NullHandler
+    logger = logging.getLogger(__app_name__)
+    logger.addHandler(NullHandler())
