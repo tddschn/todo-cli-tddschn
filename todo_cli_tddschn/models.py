6657c23714a0d0bdaa018108b15185616ad9a579
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-04-22 15:43:37 +0800

    create models

--- todo_cli_tddschn/models.py
+++ todo_cli_tddschn/models.py
@@ -1,11 +1,25 @@
+from datetime import datetime
 from sqlmodel import Field, Relationship, SQLModel
 
-class TodoBase(SQLModel):
-	description: str
-	priority: str = Field(default='medium')
-	status: str = Field(default='todo')
-	project = Column(String)
-	tags = Column(String)
-	due_date = Column('Due Date', DateTime, nullable=True)
 
-class Todo(SQLModel, table=True):
+class TodoBase(SQLModel):
+    description: str
+    priority: str = Field(default='medium')
+    status: str = Field(default='todo')
+    tags: str | None
+    due_date: datetime | None
+    project_id: int | None
+
+
+class Todo(TodoBase, table=True):
+    id: int | None
+    project: 'Project' | None = Relationship(back_populates='todos')
+
+
+class ProjectBase(SQLModel):
+    name: str
+
+
+class Project(ProjectBase, table=True):
+    id: int | None
+    todos: Todo | None = Relationship(back_populates='project')
