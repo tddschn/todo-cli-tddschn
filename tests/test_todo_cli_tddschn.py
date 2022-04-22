4659144d5d4c1bf5418cec504441573f3fd3e554
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-04-22 20:46:03 +0800

    test: TestTodo class

--- tests/test_todo_cli_tddschn.py
+++ tests/test_todo_cli_tddschn.py
@@ -1,12 +1,25 @@
+import os
+import tempfile
 from todo_cli_tddschn import __version__, __app_name__
 from todo_cli_tddschn.cli import app
 
 from typer.testing import CliRunner
 
-runner = CliRunner()
+# runner = CliRunner()
 
 
-def test_version():
-    result = runner.invoke(app, ["--version"])
-    assert result.exit_code == 0
-    assert f"{__app_name__} v{__version__}\n" == result.output
+class TestTodo:
+
+    @classmethod
+    def setup_class(cls):
+        cls.runner = CliRunner()
+        cls.db_path = tempfile.mkstemp(suffix=".db")[1]
+
+    @classmethod
+    def teardown_class(cls):
+        os.unlink(cls.db_path)
+
+    def test_version(self):
+        result = self.runner.invoke(app, ["--version"])
+        assert result.exit_code == 0
+        assert f"{__app_name__} v{__version__}\n" == result.output
