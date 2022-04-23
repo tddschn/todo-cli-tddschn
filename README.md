e6f909d2fa103d9aaa4fef294eb8b01f7d31b6c2
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-04-23 22:38:48 +0800

    more ls options

---
+++ README.md
@@ -0,0 +1,94 @@
+# todo-cli-tddschn
+
+A simple command-line Todo app with typer and sqlite
+
+- [todo-cli-tddschn](#todo-cli-tddschn)
+	- [Install](#install)
+		- [pipx (recommended)](#pipx-recommended)
+		- [pip](#pip)
+	- [Usage](#usage)
+		- [todo](#todo)
+		- [todo config](#todo-config)
+		- [todo ls](#todo-ls)
+	- [Screenshots](#screenshots)
+		- [ls: list all to-dos, ordered by priority and due date.](#ls-list-all-to-dos-ordered-by-priority-and-due-date)
+
+## Install
+
+### pipx (recommended)
+```
+pipx install todo-cli-tddschn
+```
+
+### pip
+```
+pip install todo-cli-tddschn
+```
+
+## Usage
+
+### todo
+```
+todo --help
+
+Usage: todo [OPTIONS] COMMAND [ARGS]...
+
+  tddschn's command line todo app
+
+Options:
+  -v, --version         Show the application's version and exit.
+  --install-completion  Install completion for the current shell.
+  --show-completion     Show completion for the current shell, to copy it or
+                        customize the installation.
+
+  --help                Show this message and exit.
+
+Commands:
+  a        Add a new to-do with a DESCRIPTION.
+  clear    Remove all to-dos.
+  config   Getting and managing the config
+  g        Get a to-do by ID.
+  info     Get infos about todos
+  init     Initialize the to-do database.
+  ls       list all to-dos, ordered by priority and due date.
+  m        Modify a to-do by setting it as done using its TODO_ID.
+  re-init  Re-initialize the to-do database.
+  rm       Remove a to-do using its TODO_ID.
+```
+### todo config
+```
+todo config --help
+
+Usage: todo config [OPTIONS] COMMAND [ARGS]...
+
+  Getting and managing the config
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  db-path  Get the path to the to-do database.
+  edit     Edit the config file.
+  path     Get the path to the config file.
+```
+
+### todo ls
+```
+todo ls --help
+
+Usage: todo ls [OPTIONS] COMMAND [ARGS]...
+
+  list all to-dos, ordered by priority and due date.
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  project  Filter to-dos by project.
+  tag      Filter to-dos by tag.
+```
+
+## Screenshots
+
+### ls: list all to-dos, ordered by priority and due date.
+
--- todo_cli_tddschn/list_command.py
+++ todo_cli_tddschn/list_command.py
@@ -23,12 +23,14 @@ def list_all() -> None:
     _list_todos(todos)
 
 
-def _list_todos(todos: list[Todo]):
+def _list_todos(todos: list[Todo], filtered: bool = False):
     todo_list = [todo_to_dict_with_project_name(x) for x in todos]
+    if filtered:
+        no_todo_found_message = "No matching to-do found."
+    else:
+        no_todo_found_message = "There are no tasks in the to-do list yet"
     if len(todo_list) == 0:
-        typer.secho("There are no tasks in the to-do list yet",
-                    fg=typer.colors.RED,
-                    err=True)
+        typer.secho(no_todo_found_message, fg=typer.colors.RED, err=True)
         raise typer.Exit()
     typer.secho("\nto-do list:\n", fg=typer.colors.BLUE, bold=True)
     table = tabulate(todo_list, headers='keys')
@@ -36,14 +38,57 @@ def _list_todos(todos: list[Todo]):
 
 
 @app.callback(invoke_without_command=True)
-def order_by_priority_then_due_date():
+def order_by_priority_then_due_date(
+        description: str = typer.Option(
+            None,
+            "--description",
+            "-d",
+        ),
+        priority: Priority = typer.Option(None,
+                                          "--priority",
+                                          "-p",
+                                          case_sensitive=False),
+        status: Status = typer.Option(None,
+                                      "--status",
+                                      "-s",
+                                      case_sensitive=False),
+        project: str = typer.Option(
+            None,
+            "--project",
+            "-pr",
+        ),
+        tags: str = typer.Option(
+            None,
+            "--tags",
+            "-t",
+        ),
+        due_date: datetime = typer.Option(
+            None,
+            "--due-date",
+            "-dd",
+        ),
+):
     """list all to-dos, ordered by priority and due date."""
     whens = {'low': 0, 'medium': 1, 'high': 2}
-    sort_logic = case(value=Todo.priority, whens=whens).label("priority")
+    wheres = []
+    if description is not None:
+        wheres.append(col(Todo.description).like(f'%{description}%'))
+    if priority is not None:
+        wheres.append(col(Todo.priority) == priority)
+    if status is not None:
+        wheres.append(col(Todo.status) == status)
+    if project is not None:
+        wheres.append(
+            col(Todo.project_id) == get_project_with_name(project).id)
+    if tags is not None:
+        wheres.append(col(Todo.tags).like(f'%{tags}%'))
+    if due_date is not None:
+        wheres.append(col(Todo.due_date) == due_date)
+    sort_logic = case(value=Todo.priority, whens=whens)
     with Session(engine) as session:
-        todos = session.query(Todo).order_by(sort_logic.desc(),
-                                             nullslast(Todo.due_date)).all()
-    _list_todos(todos)
+        todos = session.query(Todo).where(*wheres).order_by(
+            sort_logic.desc(), nullslast(Todo.due_date)).all()
+    _list_todos(todos, filtered=bool(wheres))
 
 
 @app.command('tag')
@@ -52,7 +97,7 @@ def filter_by_tags(tag: str):
     with Session(engine) as session:
         todos = session.exec(
             select(Todo).where(col(Todo.tags).like(f"%{tag}%"))).all()
-    _list_todos(todos)
+    _list_todos(todos, True)
 
 
 @app.command('project')
@@ -63,4 +108,4 @@ def filter_by_project(project_name: str):
             select(Todo).where(
                 col(Todo.project_id) == get_project_with_name(
                     project_name).id)).all()
-    _list_todos(todos)
+    _list_todos(todos, True)
