87c32fe09460ca874c7647d2bc800e7f0a39c50f
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-05-29 10:53:42 +0800

    add export_todos

--- todo_cli_tddschn/utils_commands.py
+++ todo_cli_tddschn/utils_commands.py
@@ -11,3 +11,13 @@ from . import __app_name__
 from .utils import export_todo_command
 
 app = typer.Typer(name='utils')
+
+
+@app.command('export')
+def export_todos():
+    """Export todos to todo commands that can be used to restore your todo database"""
+    # get all todo ids
+    with Session(engine) as session:
+        todo_ids: list[int] = [todo.id for todo in session.query(Todo).all()]
+    export_commands = '\n'.join(export_todo_command(todo_id) for todo_id in todo_ids)
+    typer.secho(export_commands)
