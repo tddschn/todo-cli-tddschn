d9d1c9925b5840816a4655ea31bb5cf4ec3f771b
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-05-30 11:40:45 +0800

    change to yaml config

--- todo_cli_tddschn/config.py
+++ todo_cli_tddschn/config.py
@@ -1,6 +1,6 @@
 """This module provides the RP To-Do config functionality."""
 
-import configparser
+# import configparser
 from pathlib import Path
 
 import typer
@@ -26,26 +26,32 @@ DEFAULT_FORMAT_SPEC = {
 app = typer.Typer(name='config')
 
 
-def create_default_config_object() -> configparser.ConfigParser:
-    """Create a default config object."""
-    config_parser = configparser.ConfigParser(inline_comment_prefixes='#')
-    config_parser["General"] = {"database": str(DEFAULT_DB_FILE_PATH)}
-    config_parser["Format"] = DEFAULT_FORMAT_SPEC
-    return config_parser
+def create_config_dict() -> dict:
+    """create a default config dict"""
+    d = {
+        'database': str(DEFAULT_DB_FILE_PATH),
+        'format': {
+            'due_date': DEFAULT_DUE_DATE_FORMAT,
+            'date_added': DEFAULT_DATE_ADDED_FORMAT,
+        },
+        'hide': [],
+    }
+    return d
 
 
 def init_app(db_path: Path, config_file_path: Path = CONFIG_FILE_PATH):
     """Initialize the application.
     get the config file,
     and add db_path to the config file"""
-    config_parser = create_default_config_object()
+    import yaml
+
+    config_dict = create_config_dict()
+    config_s = yaml.safe_dump(config_dict)
     try:
         touch_config_file(config_file_path)
-        with CONFIG_FILE_PATH.open("w") as file:
-            config_parser.write(file)
+        config_file_path.write_text(config_s)
         typer.secho(f"Config file created at {config_file_path}", fg=typer.colors.GREEN)
     except:
-        # raise TodoInitError(f'Creating config file {config_file_path} failed')
         typer.secho(
             f'Creating config file {config_file_path} failed', fg=typer.colors.RED
         )
@@ -57,30 +63,23 @@ def touch_config_file(config_file_path):
     config_file_path.touch(exist_ok=True)
 
 
-def read_config(config_file: Path) -> configparser.ConfigParser:
+def read_config(config_file: Path) -> dict:
+    """read the config file to get the config dict"""
+    import yaml
+
     logger.info(f"Reading config file: {config_file}")
-    config_parser = configparser.ConfigParser(inline_comment_prefixes='#')
-    config_parser.read(config_file)
-    return config_parser
+    return yaml.safe_load(config_file.read_text())
 
 
 def get_database_path(config_file: Path) -> Path:
     """Read and returns the current path to the to-do database
     from the config file."""
-    config_parser = read_config(config_file)
-    db_path = config_parser["General"]["database"]
-    logger.info(f'Read db path: {db_path}')
-    return Path(db_path)
+    return Path(read_config(config_file)["database"])
 
 
-def get_format(config_file: Path) -> configparser.SectionProxy | dict[str, str]:
+def get_format(config_file: Path) -> dict[str, str]:
     """Get format specs from the config file."""
-    config_parser = read_config(config_file)
-    try:
-        format_specs = config_parser["Format"]
-    except:
-        format_specs = DEFAULT_FORMAT_SPEC
-    return format_specs
+    return read_config(config_file)["format"]
 
 
 @app.command('path')
