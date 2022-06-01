af4482fb746c6c7d8549f7693b8264ce32610a83
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-06-01 10:32:25 +0800

    add command_completer to itodo

--- todo_cli_tddschn/todo_shell.py
+++ todo_cli_tddschn/todo_shell.py
@@ -10,14 +10,28 @@ from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 session = PromptSession()
 runner = CliRunner()
 
+registered_command_names = [x.name for x in app.registered_commands]
+registered_group_names = [x.name for x in app.registered_groups]
+
+from prompt_toolkit.completion import WordCompleter
+
+# html_completer = WordCompleter(['<html>', '<body>', '<head>', '<title>'])
+# text = prompt('Enter HTML: ', completer=html_completer)
+
+command_completer = WordCompleter(
+    list(filter(None, (registered_command_names + registered_group_names)))
+)
+
 
 def main() -> None:
     while True:
-        text = session.prompt('todo> ', auto_suggest=AutoSuggestFromHistory())
+        text = session.prompt(
+            "todo> ", auto_suggest=AutoSuggestFromHistory(), completer=command_completer
+        )
         # print('You said: %s' % text)
         result = runner.invoke(app, text, color=True)
         typer.secho(result.output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
